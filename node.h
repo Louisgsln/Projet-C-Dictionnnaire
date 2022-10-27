8821f3be3f1574b5b02bc388e4da3fcae5b4c407
ameliedx <104188657+ameliedx@users.noreply.github.com>
2022-10-27 13:32:33 +0200

    Update node.h

--- node.h
+++ node.h
@@ -1,1 +1,26 @@
+#ifndef UNTITLED_NODE_H
+#define UNTITLED_NODE_H
 
+typedef struct s_children{
+    p_node pn;
+    struct s_children *next;
+} t_children, *p_children;
+
+typedef struct s_forms{
+    //inofrmation (ex: Ver:IFut+SG+P1)
+    char infos[100];
+    //mot (ex: abandonnerai)
+    char value[100];
+    struct s_forms *next;
+} t_forms, *p_forms;
+
+
+typedef struct s_node{
+    char value[50];
+    p_children children;
+    p_forms forms;
+} *p_node, t_node;
+
+
+
+#endif //UNTITLED_NODE_H
