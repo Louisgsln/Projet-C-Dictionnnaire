c5e5052764e253ddde73e8956833dd852b311856
ameliedx <104188657+ameliedx@users.noreply.github.com>
2022-11-07 14:05:32 +0100

    Update node.h

--- node.h
+++ node.h
@@ -1,10 +1,25 @@
 #ifndef UNTITLED_NODE_H
 #define UNTITLED_NODE_H
 
+struct s_children;
+typedef struct s_children *p_children;
+struct s_forms;
+typedef struct s_forms *p_forms;
+
+
+typedef struct s_node{
+    char value;
+    p_children children;
+    p_forms forms;
+} *p_node, t_node;
+
 typedef struct s_children{
     p_node pn;
     struct s_children *next;
-} t_children, *p_children;
+} t_children;
+
+typedef p_children LISTE;
+
 
 typedef struct s_forms{
     //inofrmation (ex: Ver:IFut+SG+P1)
@@ -12,14 +27,11 @@ typedef struct s_forms{
     //mot (ex: abandonnerai)
     char value[100];
     struct s_forms *next;
-} t_forms, *p_forms;
+} t_forms;
 
-
-typedef struct s_node{
-    char value[50];
-    p_children children;
-    p_forms forms;
-} *p_node, t_node;
+p_node createNode(char);
+int recherche_lettre_children(LISTE, char);
+p_children createChildren(p_node);
 
 
 
