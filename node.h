fc1160caf2eeaa4007c43d99a4b89e803dbc9bf9
ameliedx <104188657+ameliedx@users.noreply.github.com>
2022-11-13 20:48:47 +0100

    Update node.h

--- node.h
+++ node.h
@@ -1,6 +1,6 @@
 #ifndef UNTITLED_NODE_H
 #define UNTITLED_NODE_H
-
+#define TAILLE_MAX 1000
 struct s_children;
 typedef struct s_children *p_children;
 struct s_forms;
@@ -23,23 +23,25 @@ typedef p_children LISTE;
 
 typedef struct s_forms{
     //inofrmation (ex: Ver:IFut+SG+P1)
-    char infos[100];
+    char infos[TAILLE_MAX];
     //mot (ex: abandonnerai)
-    char value[100];
+    char value[TAILLE_MAX];
     struct s_forms *next;
 } t_forms;
 
 p_node createNode(char);
 int recherche_lettre_children(LISTE, char);
 p_children createChildren(p_node);
-LISTE ajouterEnFin(LISTE, p_node);
+LISTE ajouterEnFinChildren(LISTE, p_node);
 void afficherListe(LISTE );
-LISTE ajouter_mot_arbre(LISTE, char*);
+LISTE ajouter_mot_arbre(LISTE, char*, char*, char*);
 void afficher_enfants(LISTE );
 int recherche_lettre(p_children, char);
-void rentrer_mot_arbre_entier(p_node, char *);
+void rentrer_mot_arbre_entier(p_node, char *, char*, char*);
 int recherche_mot(p_node,  char *);
 p_children recherche_enfant(p_children, char);
-
+p_forms createForms(char *, char *);
+p_forms ajouterEnFinForms(p_forms , char *, char *);
+void affichermots_flechis(p_forms );
 
 #endif //UNTITLED_NODE_H
