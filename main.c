a48bafa5a2b25e1b10004ebde3e7c9b3123f0bf6
ameliedx <104188657+ameliedx@users.noreply.github.com>
2022-11-13 11:42:46 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,26 +4,12 @@
 #include <ctype.h>
 #include "node.h"
 #include "bintree.h"
+#include "fichier.h"
 
 #define TAILLE_MAX 1000 // Tableau de taille 1000
 
-int main(int argc, char *argv[])
+void remplis_mots_coupes(char mots_coupes[3][TAILLE_MAX], char *chaine)
 {
-    FILE* fichier = NULL;
-    char chaine[TAILLE_MAX] = ""; // Chaîne vide de taille TAILLE_MAX
-
-    fichier = fopen("C:\\Users\\ameli\\CLionProjects\\Dictionnaire\\dico_10_lignes.txt", "r");
-
-    if (fichier != NULL) {
-        fgets(chaine, TAILLE_MAX, fichier); // On lit maximum TAILLE_MAX caractères du fichier, on stocke le tout dans "chaine"
-        printf("%s", chaine); // On affiche la chaîne
-        //while (fgets(chaine, TAILLE_MAX, fichier) != NULL)
-            //printf("%s", chaine); // On affiche la chaîne
-        fclose(fichier);
-    }
-
-    //couper la  ligne
-    char mots_coupes[3][TAILLE_MAX];
     char mot[TAILLE_MAX];
     int pos_chaine =0;
     int pos_mot =0;
@@ -37,15 +23,126 @@ int main(int argc, char *argv[])
         mot[pos_mot] = '\0';
         pos_mot = 0;
         strcpy(mots_coupes[numero_mot], mot);
-        printf("'%s'\n", mot);
+        //printf("'%s'\n", mot);
+
+        while(isspace(chaine[pos_chaine])) {
+            pos_chaine++;
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    FILE* fichier = NULL;
+    char chaine[TAILLE_MAX] = ""; // Chaîne vide de taille TAILLE_MAX
+    t_tree t_nom, t_verbe, t_adj, t_adv;
+    t_nom.root = createNode('/');
+    t_verbe.root = createNode('/');
+    t_adv.root = createNode('/');
+    t_adj.root = createNode('/');
+
+    fichier = fopen("C:\\Users\\ameli\\CLionProjects\\Dictionnaire\\dico_10_lignes.txt", "r");
+
+    if (fichier != NULL) {
+        //fgets(chaine, TAILLE_MAX, fichier); // On lit maximum TAILLE_MAX caractères du fichier, on stocke le tout dans "chaine"
+        //printf("%s", chaine); // On affiche la chaîne
+        while (fgets(chaine, TAILLE_MAX, fichier) != NULL) {
+            printf("\n%s ", chaine); // On affiche la chaîne
+            //couper la  ligne
+            char mots_coupes[3][TAILLE_MAX];
+
+            remplis_mots_coupes(mots_coupes, chaine);
+
+            //trouver quel type d'arbre
+            int res = trouver_arbre(mots_coupes[2]);
+
+            if (res ==1){
+                rentrer_mot_arbre_entier(t_nom.root, mots_coupes[1]);
+                afficher_enfants(t_nom.root->children);
+                printf("\n");
+            }
+            if (res == 2){
+                rentrer_mot_arbre_entier(t_verbe.root, mots_coupes[1]);
+                afficher_enfants(t_verbe.root->children);
+                printf("\n");
+            }
+            if (res == 3){
+                rentrer_mot_arbre_entier(t_adj.root, mots_coupes[1]);
+                afficher_enfants(t_adj.root->children);
+                printf("\n");
+            }
+            if (res == 4){
+                rentrer_mot_arbre_entier(t_adv.root, mots_coupes[1]);
+                afficher_enfants(t_adv.root->children);
+                printf("\n");
+            }
+        }
+        fclose(fichier);
+    }
+
+    printf("%d\n", recherche_mot(t_nom.root, "stabilimetre"));
+
+    printf("%d\n", recherche_mot(t_nom.root, "stabilisant"));
+
+    printf("%d\n", recherche_mot(t_nom.root, "stabiliser"));
+
+
+
+    //couper la  ligne
+    /*char mots_coupes[3][TAILLE_MAX];
+    char mot[TAILLE_MAX];
+    int pos_chaine =0;
+    int pos_mot =0;
+    for (int numero_mot =0; numero_mot<3; numero_mot++) {
+        while (isspace(chaine[pos_chaine])== 0) {
+            mot[pos_mot] = chaine[pos_chaine];
+            pos_mot++;
+            pos_chaine++;
+        }
+
+        mot[pos_mot] = '\0';
+        pos_mot = 0;
+        strcpy(mots_coupes[numero_mot], mot);
+        //printf("'%s'\n", mot);
 
         while(isspace(chaine[pos_chaine])) {
             pos_chaine++;
         }
     }
 
+
     //trouver quel type d'arbre
-    printf("%s\n", mots_coupes[2]);
+    int res = trouver_arbre(mots_coupes[2]);
+    t_tree t_nom, t_verbe, t_adj, t_adv;
+    t_nom.root = t_verbe.root = t_adv.root = t_adj.root = createNode('/');
+
+    if (res ==1){
+        rentrer_mot_arbre_entier(t_nom.root, mots_coupes[1]);
+        afficher_enfants(t_nom.root->children);
+        printf("\n");
+    }
+    if (res == 2){
+        rentrer_mot_arbre_entier(t_verbe.root, mots_coupes[1]);
+        afficher_enfants(t_verbe.root->children);
+        printf("\n");
+
+    }
+    if (res == 3){
+
+        rentrer_mot_arbre_entier(t_adj.root, mots_coupes[1]);
+        afficher_enfants(t_adj.root->children);
+        printf("\n");
+
+    }
+    if (res == 4){
+
+        rentrer_mot_arbre_entier(t_adv.root, mots_coupes[1]);
+        afficher_enfants(t_adv.root->children);
+        printf("\n");
+
+    }*/
+
+    /*printf("%s\n", mots_coupes[2]);
     int pos3_mot=0;
     char type_mot[TAILLE_MAX];
     char type_Nom[4] = { 'N', 'o', 'm', '\0' };
@@ -70,21 +167,32 @@ int main(int argc, char *argv[])
     }
     if(strcmp(type_mot,type_Adv) == 0){
         printf("dans arbre adverbe\n");
-    }
+    }*/
 
 
 
-    LISTE L;
+    /*LISTE L;
     p_node pn;
     t_tree t;
-    t.root = createNode('/');
+    t.root = createNode('/');*/
 
+
+    /*
     //ajoute lettre dans liste_children du p_node precedent
     t.root->children = ajouter_mot_arbre(t.root->children, mots_coupes[1]);
     printf("\n");
-    //affiche toutes les listes des enfants non nulles 
+    //affiche toutes les listes des enfants non nulles
     afficher_enfants(t.root->children);
 
+    if (recherche_lettre(t.root ->children->pn->children->pn->children->pn->children, mots_coupes[1][3])){
+        printf("\noui %c existe", mots_coupes[1][3]);
+    }
+    else{
+        printf("\nnon %c existe pas", mots_coupes[1][3]);
+    }*/
+
+    /*rentrer_mot_arbre_entier(t.root, mots_coupes[1]);
+    afficher_enfants(t.root->children);*/
 
 
     return 0;
